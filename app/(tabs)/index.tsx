d8a6f1074fb33935dc77ff0db9d37de34e2f281f
Rumen Neshev <rstar2@abv.bg>
2026-08-04 16:10:57 +0300

    asd

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { FlatList, View, Image } from "react-native";
 
 import ScreenBase from "@/app/components/ScreenBase";
 import Text from "@/app/components/Text";
 import ListHeading from "@/app/components/ListHeading";
 import PlaceCard from "@/app/components/PlaceCard";
-import { formatCurrency, formatDateTime } from "@/lib/utils";
+import { formatCurrency, formatDateTime, noop } from "@/lib/utils";
 import { HOME_BALANCE, HOME_SUBSCRIPTIONS } from "@/constants/data";
 import { Subscription } from "@/lib/types";
 import images from "@/constants/images";
+
+const user = {
+  displayName: "John Doe",
+  imageUrl: null,
+};
 
 export default function Index() {
   const [expandedPlaceId, setExpandedPlaceId] = useState<string>();
@@ -28,65 +33,10 @@ export default function Index() {
     // );
   };
 
-  const user = {
-    displayName: "John Doe",
-    imageUrl: null,
-  };
   return (
     <ScreenBase>
       <FlatList
-        ListHeaderComponent={() => (
-          <>
-            <View className="home-header">
-              <View className="home-user">
-                <Image
-                  source={
-                    user?.imageUrl ? { uri: user.imageUrl } : images.avatar
-                  }
-                  className="home-avatar"
-                />
-                <Text className="home-user-name">{user.displayName}</Text>
-              </View>
-
-              {/* <Pressable onPress={() => setIsModalVisible(true)}>
-                <Image source={icons.add} className="home-add-icon" />
-              </Pressable> */}
-            </View>
-
-            <View className="home-balance-card">
-              <Text className="home-balance-label">Balance</Text>
-
-              <View className="home-balance-row">
-                <Text className="home-balance-amount">
-                  {formatCurrency(HOME_BALANCE.amount)}
-                </Text>
-                <Text className="home-balance-date">
-                  {formatDateTime(HOME_BALANCE.nextRenewalDate, "MM/DD")}
-                </Text>
-              </View>
-            </View>
-
-            <View className="mb-5">
-              <ListHeading title="Upcoming" />
-
-              <FlatList
-                data={HOME_SUBSCRIPTIONS}
-                renderItem={({ item }) => <PlaceCard {...item} />}
-                keyExtractor={(item) => item.id}
-                ItemSeparatorComponent={() => <View className="w-4" />}
-                ListEmptyComponent={
-                  <Text className="home-empty-state">
-                    No upcoming renewals yet.
-                  </Text>
-                }
-                horizontal
-                showsHorizontalScrollIndicator={false}
-              />
-            </View>
-
-            <ListHeading title="All Places" />
-          </>
-        )}
+        ListHeaderComponent={() => <Header />}
         data={HOME_SUBSCRIPTIONS}
         extraData={expandedPlaceId}
         keyExtractor={(item) => item.id}
@@ -105,5 +55,64 @@ export default function Index() {
         contentContainerClassName="pb-30"
       />
     </ScreenBase>
+  );
+}
+
+// NOTE: Extract this to a separate component as otherwise it messes
+// the rendering. When the "big" flat list is rerendered, like when expanding a place,
+// the ListHeaderComponent is rerendered and thus the horizontal FlatList and some layout logic breaks as the refs are not same, etc... (I couldn't understand it fully)
+// This is a known issue with FlatList in React Native.
+// BUT, just extracting it to a separate component fixes the issue, as the horizontal FlatList is not rerendered when the "big" FlatList is rerendered.
+// Also note that the react-compiler is ON, so it does the memoization for use
+function Header() {
+  return (
+    <>
+      <View className="home-header">
+        <View className="home-user">
+          <Image
+            source={user?.imageUrl ? { uri: user.imageUrl } : images.avatar}
+            className="home-avatar"
+          />
+          <Text className="home-user-name">{user.displayName}</Text>
+        </View>
+
+        {/* <Pressable onPress={() => setIsModalVisible(true)}>
+                <Image source={icons.add} className="home-add-icon" />
+              </Pressable> */}
+      </View>
+
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Balance</Text>
+
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">
+            {formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className="home-balance-date">
+            {formatDateTime(HOME_BALANCE.nextRenewalDate, "MM/DD")}
+          </Text>
+        </View>
+      </View>
+
+      <View className="mb-5">
+        <ListHeading title="Upcoming" />
+
+        <FlatList
+          data={HOME_SUBSCRIPTIONS}
+          renderItem={({ item }) => (
+            <PlaceCard {...item} expanded={false} onPress={noop} />
+          )}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => <View className="w-4" />}
+          ListEmptyComponent={
+            <Text className="home-empty-state">No upcoming renewals yet.</Text>
+          }
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+
+      <ListHeading title="All Places" />
+    </>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 import { twMerge } from "tailwind-merge";
-import {clsx,  ClassValue } from "clsx";
+import { clsx, ClassValue } from "clsx";
+
+export function noop() {}
 
 export function formatCurrency(value: number, currency = "USD"): string {
   try {
@@ -19,17 +21,14 @@ export function formatDateTime(value?: string, format = "MM/DD/YYYY"): string {
   if (!value) return "Not provided";
 
   const parsedDate = dayjs(value);
-  return parsedDate.isValid()
-    ? parsedDate.format(format)
-    : "Not provided";
+  return parsedDate.isValid() ? parsedDate.format(format) : "Not provided";
 }
 
 export function formatStatusLabel(value?: string): string {
   if (!value) return "Unknown";
-  
-  return value.charAt(0).toUpperCase() + value.slice(1);
-};
 
+  return value.charAt(0).toUpperCase() + value.slice(1);
+}
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
